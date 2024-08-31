5915cda3cfd29b2f7c23ea30430a7aa82d5e288e
Tetsu Yama <tsk.blog.2021@gmail.com>
2024-08-31 13:36:04 +0900

    sdfdfdf

--- 10_infra/elb.tf
+++ 10_infra/elb.tf
@@ -17,3 +17,63 @@ resource "aws_lb" "front" {
   #   aws_acm_certificate.tokyo_cert
   # ]
 }
+
+# ---------------------------------------------
+# Listener
+# ---------------------------------------------
+resource "aws_lb_listener" "front_http" {
+  load_balancer_arn = aws_lb.front.arn
+  protocol          = "HTTP"
+  port              = 80
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.webapp_blue.arn
+  }
+
+  lifecycle {
+    ignore_changes = [default_action]
+  }
+}
+
+
+# ---------------------------------------------
+# Target Group
+# ---------------------------------------------
+resource "aws_lb_target_group" "webapp_blue" {
+  name        = "${var.project}-${var.environment}-webapp-blue-tg"
+  vpc_id      = aws_vpc.vpc.id
+  protocol    = "HTTP"
+  port        = 3000
+  target_type = "ip"
+
+  health_check {
+    port = 3000
+    path = "/"
+  }
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-webapp-tg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "webapp_green" {
+  name        = "${var.project}-${var.environment}-webapp-green-tg"
+  vpc_id      = aws_vpc.vpc.id
+  protocol    = "HTTP"
+  port        = 3000
+  target_type = "ip"
+
+  health_check {
+    port = 3000
+    path = "/"
+  }
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-webapp-green-tg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
